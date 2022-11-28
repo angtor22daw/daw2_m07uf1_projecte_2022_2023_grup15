3c0562908c6d9fd15d6275bbb0c25e9bd06e211a
angtor22daw <angtor22daw@proton.me>
2022-11-28 15:37:13 +0100

    login mejorado

--- login.php
+++ login.php
@@ -18,30 +18,50 @@
 			header("Location: error_login.php");
 		}					
 	}		
-?>
+	
 ?>
 <!DOCTYPE html>
 <html lang="ca">
 	<head>
 		<meta charset="utf-8">
 		<title>Visualitzador de l'aplicació</title>
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+		<!-- CSS only -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/estils.css">
 	</head>
 	<body>
-		<h3><b>Inici de sessió del visualitzador de l'aplicació</b></h3>
-       <form action="login.php" method="POST">
-			<p>Indica el teu nom d'usuari: <input type="text" name="usuari"></p>
-			<p>Indica la teva contrasenya: <input type="password" name="password"></p>
-			<input type="submit" value="Envia">
-		</form>
-		<p><a href="index.php">Torna a la pàgina inicial</a></p>
-        <label class="diahora"> 
-        <?php
-			date_default_timezone_set('Europe/Andorra');
-			echo "<p>Data i hora: ".date('d/m/Y h:i:s')."</p>";						
-        ?>
-         <label class="diahora"> 
+		<!-- JavaScript Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+	<section class="vh-100 gradient-custom">
+		<div class="container py-5 h-100">
+			<div class="row d-flex justify-content-center align-items-center h-100">
+			<div class="col-12 col-md-8 col-lg-6 col-xl-5">
+				<div class="card shadow-2-strong" style="border-radius: 1rem;">
+				<div class="card-body p-5 text-center">
+
+					<h3 class="mb-5">Iniciar Sessió</h3>
+
+					<form action="login.php" method="POST">
+						<div class="form-outline mb-4">
+							<input type="text" name="usuari" id="typeEmailX-2" class="form-control form-control-lg" placeholder="Usuari"/>
+						</div>
+
+						<div class="form-outline mb-4">
+							<input type="password" name="ctsnya" id="typePasswordX-2" class="form-control form-control-lg" placeholder="Contrasenya"/>
+						</div>
+
+						<button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
+					</form>
+					<hr class="my-4">
+					
+					<p><a href="index.php">Torna a la pàgina inicial</a></p>
+
+				</div>	
+				</div>
+			</div>
+			</div>
+		</div>
+		</section>
 	</body>
 </html>
   

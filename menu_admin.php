a663e6d66c6705ab3cc3889da359eb9ea4d09e46
miqsem22daw <miqsem22daw@proton.me>
2022-12-14 01:05:34 +0100

    bootstrap menus

--- menu_admin.php
+++ menu_admin.php
@@ -20,22 +20,63 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Menú del administrador</title>
-		<link rel="stylesheet" href="agenda.css">
+		<!--<link rel="stylesheet" href="agenda.css"> -->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
+        <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sidebars/">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
+		<link href="css/sidebars.css" rel="stylesheet">
 	</head>
 	<body>
-		<h3><b>Menú del Administrador</b></h3>
-		<p>Gestionament dels alumnes: </p>
-        <a href="crearAlumne.php">Crear alumne</a><br>
-        <a href="visualitzarAlumnesAdmin.php">Visualitzar alumnes</a><br>
-        <a href="modificarAlumne.php">Modificar notes dels alumnes</a><br>
-        <a href="eliminarAlumne.php">Eliminar alumne</a><br>
-		<p>Creació d'usuaris de l'aplicació: </p>
-        <p><a href="registre.php">Registrar nous usuaris</a></p><br>
-		<p>Tancament de sessió: </p>
-        <p><a href="logout.php">Finalitza la sessió</a></p>
+		<div class="flex-shrink-0 p-3 bg-white" style="width: 280px;">
+			<a class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+				<span class="fs-5 fw-semibold">Menú del Administrador</span>
+			</a>
+			<ul class="list-unstyled ps-0">
+      			<li class="mb-1">
+					<button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
+						Gestionament dels alumnes
+        			</button>
+					<div class="collapse show" id="home-collapse">
+						<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+							<li><a href="crearAlumne.php" class="link-dark rounded">Crear alumne</a></li>
+							<li><a href="visualitzarAlumnesAdmin.php" class="link-dark rounded">Visualitzar alumnes</a></li>
+							<li><a href="modificarAlumne.php" class="link-dark rounded">Modificar notes dels alumnes</a></li>
+							<li><a href="eliminarAlumne.php" class="link-dark rounded">Eliminar alumne</a></li>
+						</ul>
+					</div>
+				</li>
+      			<li class="mb-1">
+				  	<button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
+					  	Creació d'usuaris de l'aplicació
+        			</button>
+        			<div class="collapse" id="dashboard-collapse">
+						<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+							<li><a href="eliminarAlumne.php" class="link-dark rounded">Registrar nous usuaris</a></li>
+						</ul>
+					</div>
+				</li>			
+      			<li class="mb-1">
+				  	<button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
+					  	Tancament de sessió
+        			</button>
+        			<div class="collapse" id="orders-collapse">
+						<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+							<li><a href="logout.php" class="link-dark rounded">Finalitza la sessió</a></li>
+						</ul>
+					</div>
+				</li>
+			</ul>
+			<!-- <a href="crearAlumne.php">Crear alumne</a><br>
+			<a href="visualitzarAlumnesAdmin.php">Visualitzar alumnes</a><br>
+			<a href="modificarAlumne.php">Modificar notes dels alumnes</a><br>
+			<a href="eliminarAlumne.php">Eliminar alumne</a><br>
+			<p>Creació d'usuaris de l'aplicació: </p>
+			<p><a href="registre.php">Registrar nous usuaris</a></p><br>
+			<p>Tancament de sessió: </p>
+			<p><a href="logout.php">Finalitza la sessió</a></p> -->
 
+		</div>
         <?php
 			// require('./biblioteca.php');
 			echo "<p>Usuari utilitzant l'agenda: ".$_SESSION['usuari']."</p>";
--- menu_basic.php
+++ menu_basic.php
@@ -17,15 +17,45 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Interfície de l'usuari</title>
-		<link rel="stylesheet" href="agenda.css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sidebars/">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
+		<link href="css/sidebars.css" rel="stylesheet">
 	</head>
 	<body>
-		<h3><b>Menú de l'usuari</b></h3>
+	<div class="flex-shrink-0 p-3 bg-white" style="width: 280px;">
+			<a class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+				<span class="fs-5 fw-semibold">Menú de l'usuari</span>
+			</a>
+			<ul class="list-unstyled ps-0">
+      			<li class="mb-1">
+					<button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
+						Visualització dels alumnes
+        			</button>
+					<div class="collapse show" id="home-collapse">
+						<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+							<li><a href="visualitzarAlumnesBasic.php" class="link-dark rounded">Visualitzar alumnes</a></li>
+						</ul>
+					</div>
+				</li>		
+      			<li class="mb-1">
+				  	<button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
+					  	Tancament de sessió
+        			</button>
+        			<div class="collapse" id="orders-collapse">
+						<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+							<li><a href="logout.php" class="link-dark rounded">Finalitza la sessió</a></li>
+						</ul>
+					</div>
+				</li>
+			</ul>
+		</div>
+		<!-- <h3><b>Menú de l'usuari</b></h3>
 		<p>Visualització dels alumnes: </p>
 		<a href="visualitzarAlumnesBasic.php">Visualitzar alumnes</a><br><br>
 		<p>Tancament de sessió: </p>
-        <p><a href="logout.php">Finalitza la sessió</a></p>
+        <p><a href="logout.php">Finalitza la sessió</a></p> -->
 
         <?php
 			// require('./biblioteca.php');
